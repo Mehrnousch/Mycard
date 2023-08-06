c4312d0b81f601311d157c7e893b2996b2f6c080
Mehrnousch <73842806+Mehrnousch@users.noreply.github.com>
2023-08-06 22:14:49 +0200

    My Card

---
+++ MyCard/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  MyCard
+//
+//  Created by mehrnoush abdinian on 06.08.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(red:0.09, green:0.63, blue:0.52)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                
+                Image("Mehrnoush")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                )
+                Text("Mehrnoush Abdinian")
+                    .font(Font.custom("Caveat-VariableFont_wght", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+49 ... ... ....", imageName: "phone.fill")
+                InfoView(text: "m.abdinian1989@gmail.com", imageName: "envelope.fill")
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyCard/MyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardApp.swift
+//  MyCard
+//
+//  Created by mehrnoush abdinian on 06.08.23.
+//
+
+import SwiftUI
+
+@main
+struct MyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
